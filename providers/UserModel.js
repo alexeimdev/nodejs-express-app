ed2c5bd33ebc57f00b628e0600474098563b8808
alexeimdev <alexeim.dev@gmail.com>
2018-06-01 16:58:48 +0300

    changed user model

---
+++ providers/UserModel.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://localhost/my_db');
+mongoose.Promise = global.Promise;
+
+
+var userSchema = new Schema({
+    gender: String,
+    name: {
+        title: String,
+        first: String,
+        last: String,
+    },
+    location: {
+        street: String,
+        city: String,
+        state: String,
+        postcode: Number
+    },
+    email: String,
+    dob: String,
+    phone: String,
+    cell: String,
+    picture: {
+        large: String,
+        medium: String,
+        thumbnail: String
+    }
+});
+
+var UserModel = mongoose.model('users', userSchema);
+
+module.exports = UserModel;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,54 @@
 var express = require('express');
+const fetch = require('node-fetch');
 var router = express.Router();
-var User = require('../providers/User');
+var UserModel = require('../providers/UserModel');
 
 /* GET users listing. */
-router.get('/:id', function (req, res, next) {
-  let id = req.params.id;
-  User.findOne({ id: id }, function(err, user) {
-    if (err) throw err;
-    res.json(user);
-  });
-});
+// router.get('/:id', function (req, res, next) {
+//     let id = req.params.id;
+//     UserModel.findOne({ id: id }, function (err, user) {
+//         if (err) throw err;
+//         res.json(user);
+//     });
+// });
 
-router.get('/insert', function (req, res, next) {
-  var newUser = new User({
-    id: req.query.id,
-    name: req.query.name,
-    age: req.query.age
-  });
-
-  newUser.save(function (err) {
-    if (err) throw err;
-    res.json('User saved successfully!');
-  });
+router.get('/', function (req, res, next) {
+    fetch('https://randomuser.me/api')
+        .then(response => response.json())
+        .then(json => json.results)
+        .then(results => results[0])
+        .then(user => {
+            const newUser = new UserModel({
+                gender: user.gender,
+                name: {
+                    title: user.name.title,
+                    first: user.name.first,
+                    last: user.name.last
+                },
+                location: {
+                    street: user.location.street,
+                    city: user.location.city,
+                    state: user.location.state,
+                    postcode: user.location.postcode
+                },
+                email: user.email,
+                dob: user.dob,
+                phone: user.phone,
+                cell: user.cell,
+                picture: {
+                    large: user.picture.large,
+                    medium: user.picture.medium,
+                    thumbnail: user.picture.thumbnail
+                }
+            });
+            newUser.save(function (err) {
+                if (err) throw err;
+                res.json('User saved successfully!');
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 });
 
 
