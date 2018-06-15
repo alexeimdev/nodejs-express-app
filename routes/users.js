6e4ba4603c61e09085be0ebe00f6c928d67a7da1
alexeimdev <alexeim.dev@gmail.com>
2018-06-15 14:17:39 +0300

    Added  inserting multiple user for the client

--- routes/users.js
+++ routes/users.js
@@ -3,57 +3,59 @@ const fetch = require('node-fetch');
 var router = express.Router();
 var UserModel = require('../providers/UserModel');
 
-/* GET users listing. */
-// router.get('/:id', function (req, res, next) {
-//     let id = req.params.id;
-//     UserModel.findOne({ id: id }, function (err, user) {
-//         if (err) throw err;
-//         res.json(user);
-//     });
-// });
+router.get('/insertnew/:numberOfResults', function (req, res, next) {
+    const numberOfResults = req.params.numberOfResults;
+    fetch(`https://randomuser.me/api/?page=1&results=${numberOfResults}&seed=abc`)
+        .then((response) => { 
+            return response.json() 
+        })
+        .then((json) => {
+            return json.results
+        })
+        .then(results => {
+            let users = [];
+            results.forEach(user => {
+                try {
+                    const newUser = new UserModel({
+                        gender: user.gender,
+                        name: {
+                            title: user.name.title,
+                            first: user.name.first,
+                            last: user.name.last
+                        },
+                        location: {
+                            street: user.location.street,
+                            city: user.location.city,
+                            state: user.location.state,
+                            postcode: user.location.postcode
+                        },
+                        email: user.email,
+                        dob: user.dob,
+                        phone: user.phone,
+                        cell: user.cell,
+                        picture: {
+                            large: user.picture.large,
+                            medium: user.picture.medium,
+                            thumbnail: user.picture.thumbnail
+                        }
+                    });
+                    users.push(newUser);
+                }
+                catch (err) {
+                    console.log(err);
+                }
+            });
 
-router.get('/insertnew', function (req, res, next) {
-    fetch('https://randomuser.me/api')
-        .then(response => response.json())
-        .then(json => json.results)
-        .then(results => results[0])
-        .then(user => {
-            try {
-                const newUser = new UserModel({
-                    gender: user.gender,
-                    name: {
-                        title: user.name.title,
-                        first: user.name.first,
-                        last: user.name.last
-                    },
-                    location: {
-                        street: user.location.street,
-                        city: user.location.city,
-                        state: user.location.state,
-                        postcode: user.location.postcode
-                    },
-                    email: user.email,
-                    dob: user.dob,
-                    phone: user.phone,
-                    cell: user.cell,
-                    picture: {
-                        large: user.picture.large,
-                        medium: user.picture.medium,
-                        thumbnail: user.picture.thumbnail
-                    }
-                });
-                newUser.save(function (err) {
-                    if (err) throw err;
-                    res.json('User saved successfully!');
-                });
-            }
-            catch (err) {
-                console.log(err);
-            }
+            UserModel.create(users, function (err) {
+                if (err) throw err;
+                res.json(`${numberOfResults} users saved successfully!`);
+            });
+            
         })
         .catch((err) => {
             throw err;
         })
+
 });
 
 router.get('/getall', function (req, res, next) {
@@ -73,7 +75,7 @@ router.get('/getall', function (req, res, next) {
 
 router.get('/getuser/:id', function (req, res, next) {
     try {
-        UserModel.find({_id: req.params.id}, function (err, docs) {
+        UserModel.find({ _id: req.params.id }, function (err, docs) {
             if (!err) {
                 const user = docs.map(item => item['_doc'])[0]
                 res.json(user);
