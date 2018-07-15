0713cdabe610308d51948ef376196119aa2ad881
alexeimdev <alexeim.dev@gmail.com>
2018-07-15 17:57:04 +0300

    Update fetchUsers

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,9 +1,11 @@
+import * as types from './actionTypes';
+
 export function fetchUsers() {
     debugger;
-    return function (dispatch) {
+    return (dispatch) => {
         fetch('http://localhost:3000/users/getall')
             .then(res => res.json())
-            .then(users => dispatch(users))
-            .catch(err => dispatch(err))
+            .then(users => dispatch({ type: types.FETCHING_SUCCEDED, payload: users }))
+            .catch(err => dispatch({ type: types.FETCHING_SUCCEDED, payload: err }))
     }
 }
--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,11 +1,13 @@
 import initialState from './initialState';
 import * as types from '../actions/actionTypes';
+import * as actions from '../actions/actions';
 
 export default function counterReducer (state = initialState.users, action) {
+    debugger;
     const prevState = state;
     switch (action.type) {
         case types.FETCHING: {
-            return [];
+            return actions.fetchUsers();
         }
         case types.FETCHING_SUCCEDED: {
             return action.payload;
